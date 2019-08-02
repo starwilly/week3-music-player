bdcf201e7e2517dac1444a48e6d122656751c8bd
starwilly <willystar@gmail.com>
2019-08-03 00:11:50 +0800

    add player view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PlayerViewComponent} from './views/player-view/player-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', component: PlayerViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerViewComponent } from './views/player-view/player-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerViewComponent
   ],
   imports: [
     BrowserModule,
