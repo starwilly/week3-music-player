53bc1cd5b55aca52e7289466ec41f25b24325248
starwilly <willystar@gmail.com>
2019-08-03 01:47:12 +0800

    add album-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerViewComponent } from './views/player-view/player-view.component';
 import { AlbumListComponent } from './components/album-list/album-list.component';
+import { AlbumDetailComponent } from './components/albumn-detail/album-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerViewComponent,
-    AlbumListComponent
+    AlbumListComponent,
+    AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
