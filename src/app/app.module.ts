640369d1593744e1cf5c1efb7f11133dfdd93a61
starwilly <willystar@gmail.com>
2019-08-03 10:49:19 +0800

    add player component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PlayerViewComponent } from './views/player-view/player-view.component';
 import { AlbumListComponent } from './components/album-list/album-list.component';
 import { AlbumDetailComponent } from './components/albumn-detail/album-detail.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerViewComponent,
     AlbumListComponent,
-    AlbumDetailComponent
+    AlbumDetailComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
