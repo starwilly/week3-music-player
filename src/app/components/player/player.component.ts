67271443312c20ddf75989df491dd67b2864af8c
starwilly <willystar@gmail.com>
2019-08-04 15:57:22 +0800

    use slider in player

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -11,16 +11,21 @@ export class PlayerComponent implements OnInit {
   @Input() isShuffle: boolean;
   @Input() isPlaying: boolean;
   @Input() mode: PlayMode;
+  @Input() duration: number;
+  @Input() currentTime: number;
+
   @Output() modeChange = new EventEmitter<void>();
   @Output() toggleShuffle = new EventEmitter<void>();
-  @Output() playClick = new EventEmitter<void >();
-  @Output() pauseClick = new EventEmitter<void >();
-  @Output() nextClick = new EventEmitter<void >();
-  @Output() prevClick = new EventEmitter<void >();
+  @Output() playClick = new EventEmitter<void>();
+  @Output() pauseClick = new EventEmitter<void>();
+  @Output() nextClick = new EventEmitter<void>();
+  @Output() prevClick = new EventEmitter<void>();
+  @Output() jump = new EventEmitter<number>();
 
   PlayMode = PlayMode;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
